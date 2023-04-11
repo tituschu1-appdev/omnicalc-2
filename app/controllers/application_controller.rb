8d3c84638099353d4bbf85104a24b5b5460a8ef7
Titus Chu <tituschu@uchicago.edu>
2023-04-11 16:18:30 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
 class ApplicationController < ActionController::Base
+  def calculate_add
 
+    render({ :template => "calculation_forms/add_form.html.erb"})
+  end
+
+  def calculate_subtract
+
+    render({ :template => "calculation_forms/subtract_form.html.erb"})
+  end
+
+  def calculate_multiply
+
+    render({ :template => "calculation_forms/multiply_form.html.erb"})
+  end
+
+  def calculate_divide
+
+    render({ :template => "calculation_forms/divide_form.html.erb"})
+  end
+
+  def add_result
+
+    render({ :template => "calculation_forms/add_result_form.html.erb"})
+  end
+
+  def subtract_result
+
+    render({ :template => "calculation_forms/subtract_result_form.html.erb"})
+  end
+
+  def multiply_result
+
+    render({ :template => "calculation_forms/multiply_result_form.html.erb"})
+  end
+
+  def divide_result
+
+    render({ :template => "calculation_forms/divide_result_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get("/wizard_add", { :controller => "application", :action => "add_result"})
+  
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_result"})
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_result"})
+
+  get("/wizard_divide", { :controller => "application", :action => "divide_result"})
+
+  get("/add", { :controller => "application", :action => "calculate_add"})
+  
+  get("/subtract", { :controller => "application", :action => "calculate_subtract"})
+
+  get("/multiply", { :controller => "application", :action => "calculate_multiply"})
+
+  get("/divide", { :controller => "application", :action => "calculate_divide"})
+  end
+  
