91852378a9c16da4a8e97ba10d82ad3325a3adc9
Titus Chu <tituschu@uchicago.edu>
2023-04-11 16:34:11 +0000

    finsihed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   def calculate_add
-
     render({ :template => "calculation_forms/add_form.html.erb"})
   end
 
@@ -20,22 +19,30 @@ class ApplicationController < ActionController::Base
   end
 
   def add_result
-
+    @num_first = params.fetch("first_num").to_f
+    @num_second = params.fetch("second_num").to_f
+    @sum_of_num = @num_second + @num_first
     render({ :template => "calculation_forms/add_result_form.html.erb"})
   end
 
   def subtract_result
-
+    @num_first = params.fetch("first_num").to_f
+    @num_second = params.fetch("second_num").to_f
+    @sum_of_num = @num_second - @num_first
     render({ :template => "calculation_forms/subtract_result_form.html.erb"})
   end
 
   def multiply_result
-
+    @num_first = params.fetch("first_num").to_f
+    @num_second = params.fetch("second_num").to_f
+    @sum_of_num = @num_second * @num_first
     render({ :template => "calculation_forms/multiply_result_form.html.erb"})
   end
 
   def divide_result
-
+    @num_first = params.fetch("first_num").to_f
+    @num_second = params.fetch("second_num").to_f
+    @sum_of_num = @num_first/ @num_second
     render({ :template => "calculation_forms/divide_result_form.html.erb"})
   end
 end
